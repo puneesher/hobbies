bdb91b14fe9957e4564dbd7d6a3ad3b26fc129a4
Inaqui Medina <a01840708@tec.mx>
2026-06-27 23:34:28 -0600

    Fixing build

--- simple-query/src/index.js
+++
@@ -1,9 +0,0 @@
-export { getOracleConfig, getOracleConnection, getOraclePool } from './connection.js'
-export {
-  createDocument,
-  deleteDocument,
-  getDocumentById,
-  getDocuments,
-  replaceDocument,
-} from './collections.js'
-export { query, runQuery } from './query.js'
